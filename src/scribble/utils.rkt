ceac90d87676dfab0e446b105a22b3495d3db70f
Panadestein <rpana92@gmail.com>
2023-02-12 20:21:28 +0100

    Almost finished 1.14

--- src/scribble/utils.rkt
+++ src/scribble/utils.rkt
@@ -1,9 +1,15 @@
 #lang racket
 
-(require scribble/examples)
+(require scribble/examples
+         pict
+         pict/tree-layout)
+
 (provide my-eval-sicp
          my-eval-racket
-         my-eval-lazy)
+         my-eval-lazy
+         tree-drawer)
+
+;; Evaluators to be used in Scribble examples
 
 (define my-eval-sicp
   (make-base-eval
@@ -19,3 +25,27 @@
   (make-base-eval
    #:pretty-print? #t
    #:lang 'lazy))
+
+;; Generate bracktrace graphs
+
+(define (make-node sexp-atom)
+  (cc-superimpose
+   (disk 25 #:color "white")
+   (text (if (symbol? sexp-atom)
+             (symbol->string sexp-atom)
+             (number->string sexp-atom)))))
+
+(define (make-tree make-atom-node sexp)
+  (cond ((null? sexp) #f) 
+        ((list? sexp)
+         (match sexp
+           ((cons func arguments)
+            (apply tree-layout
+                   #:pict (make-atom-node func)
+                   (map (curry make-tree make-atom-node) arguments)))
+           (_ #f)))
+        (else
+         (tree-layout #:pict (make-atom-node sexp)))))
+
+(define (tree-drawer sexp)
+  (make-tree make-node sexp))
